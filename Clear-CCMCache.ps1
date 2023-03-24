7fafbc60e8899b69e187297906d1a43db3d5e5d1
VirtualOx <37578273+virtualox@users.noreply.github.com>
2023-03-24 17:14:33 +0100

    Added synopsis

--- Clear-CCMCache.ps1
+++ Clear-CCMCache.ps1
@@ -1,3 +1,42 @@
+<#
+.SYNOPSIS
+Clears old and unused content from the CCMCache folder.
+
+.DESCRIPTION
+This script detects and removes old and unused content from the CCMCache folder based on the specified number of days. It helps manage the storage space used by the CCMCache.
+
+.PARAMETER Detect
+Detects old and unused content in the CCMCache folder without deleting.
+
+.PARAMETER Clean
+Cleans old and unused content from the CCMCache folder. If no option is provided, this is the default.
+
+.PARAMETER Help
+Shows the help message.
+
+.PARAMETER Days
+Specifies the number of days to keep files in the CCMCache folder. Default is 30 days.
+
+.EXAMPLE
+.\Clear-CCMCache.ps1 -Detect
+
+.EXAMPLE
+.\Clear-CCMCache.ps1 -Clean
+
+.EXAMPLE
+.\Clear-CCMCache.ps1 -Help
+
+.EXAMPLE
+.\Clear-CCMCache.ps1 -Clean -Days 14
+
+.EXAMPLE
+.\Clear-CCMCache.ps1 -Detect -Days 14
+
+.NOTES
+This script is designed for administrators and developers who need to manage the CCMCache folder storage space.
+
+#>
+
 param(
     [switch]$Detect,
     [switch]$Clean,
